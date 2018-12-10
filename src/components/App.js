dd36c61a4fc9abc5830ae0ea02e8578b8ab76620
Pavel <Pawelv9@gmail.com>
2018-12-10 20:39:30 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
+
+const App = () =>  {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
+
+export default App;
